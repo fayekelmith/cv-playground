9022cc2c7930b48d631c3131917c0370dd4cbb5c
Faye Kelmith <fayekelmith@Kelmith.local>
2025-04-15 13:01:33 +0530

    added more utilities and discovered how it works

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import streamlit as st
-from src.components.sidebar import sidebar
-from src.components.details import details
 from src.components.dashboard import dashboard
-st.set_page_config(page_title="Computer Vision Playground", page_icon="🔭", layout="wide")
+from src.components.details import details
+from src.components.sidebar import sidebar
+import streamlit as st
+import sys
+import os
+
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+st.set_page_config(page_title="Computer Vision Playground",
+                   page_icon="🔭", layout="wide")
 
 sidebar()
 
@@ -10,7 +17,10 @@ dashboard()
 
 details()
 
-def local_css(file_path:str):
+
+def local_css(file_path: str):
     with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
 local_css("style.css")
